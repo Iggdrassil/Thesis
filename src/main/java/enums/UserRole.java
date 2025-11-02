b64e500363928bb00406b5340c5ae314db7134d4
Navar <email@gmail.com>
2025-11-02 20:23:27 +0500

    Добавил локальзованное отображение роли

--- src/main/java/enums/UserRole.java
+++ src/main/java/enums/UserRole.java
@@ -1,7 +1,22 @@
 package enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
-    ADMIN,
-    USER,
-    AUDITOR
+    ADMIN("Администратор ИБ"),
+    USER("Пользователь"),
+    AUDITOR("Аудитор");
+
+    private final String roleName;
+
+    UserRole(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @JsonValue
+    public String getRoleName() {
+        return roleName;
+    }
 }
