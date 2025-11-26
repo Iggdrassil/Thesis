f911b372ea7791fc9b2fe539b20259fb13ebeb35
Navar <email@gmail.com>
2025-11-26 21:22:06 +0500

    Добавил папку other

--- src/main/java/run/AppRun.java
+++ src/main/java/run/AppRun.java
@@ -3,7 +3,7 @@ package run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"run", "controllers", "database", "security"})
+@SpringBootApplication(scanBasePackages = {"run", "controllers", "database", "security", "services", "other"})
 public class AppRun {
     public static void main(String[] args) {
         SpringApplication.run(AppRun.class, args);
